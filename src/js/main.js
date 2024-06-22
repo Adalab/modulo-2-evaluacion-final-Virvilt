9c2676f4b6ae61775ce49ba3872348303322d958
Virginia <virginia.vilchez.t@gmail.com>
2024-06-22 22:24:12 +0200

    First Commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// QUERY SELECTOR
+
+const searchButton = document.querySelector(".js__searchButton");
+const paletteInput = document.querySelector(".js__searchInput");
+const cardsUl = document.querySelector(".js__cardsUl");
+const favouritesUl = document.querySelector(".js__favouritesUl");
+
+// DATOS
+
+// FUNCIONES
+
+// FUNCIONES DE EVENTOS
+
+// EVENTOS
+
+// CÓDIGO CUANDO CARGA LA PÁGINA
